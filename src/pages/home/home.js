b17ab7051f9762b886d374cf5b7820e59a297672
clebercmsf <cleber.cmsf@gmail.com>
2023-05-11 10:27:17 -0300

    dark-mode fix

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,15 +4,23 @@ const chk = document.getElementById("chk")
 chk.addEventListener("change", () => {
   document.body.classList.toggle("dark-mode");
   switchLogo();
+  switchLogo1();
 })
 
 // switchLogo
-var logoLight = "../../assets/img/telecall-logo4-light.png";
-var logoDark = "../../assets/img/telecall-logo4-dark.png";
+var logoLight = "../../assets/img/telecall-logo6-light.png";
+var logoDark = "../../assets/img/telecall-logo6-dark.png";
 
-// function switchLogo() {
-// document.getElementById("logo").src = logoDark;
-// let aux = logoDark;
-// logoDark = logoLight;
-// logoLight = aux;
-// }
+function switchLogo() {
+document.getElementById("logo").src = logoDark;
+let aux = logoDark;
+logoDark = logoLight;
+logoLight = aux;
+}
+
+function switchLogo1() {
+  document.getElementById("logo1").src = logoDark;
+  let aux = logoDark;
+  logoDark = logoLight;
+  logoLight = aux;
+}
