fc84f3cd7e4608cd912e094a26c98dfe5767a5e1
clebercmsf <cleber.cmsf@gmail.com>
2023-05-12 01:24:04 -0300

    darkmode fix

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,33 +1,3 @@
-// dark-mode
-const chk = document.getElementById("chk")
-
-chk.addEventListener("change", () => {
-  document.body.classList.toggle("dark-mode");
-  switchLogoHeader();
-  switchLogoFooter();
-})
-
-// switchLogo
-var logoLightHeader = "../../assets/img/telecall-logo6-light.png";
-var logoDarkHeader = "../../assets/img/telecall-logo6-dark.png";
-
-function switchLogoHeader() {
-  document.getElementById("logo-header").src = logoDarkHeader;
-  let aux = logoDarkHeader;
-  logoDarkHeader = logoLightHeader;
-  logoLightHeader = aux;
-}
-
-var logoLightFooter = "../../assets/img/telecall-logo6-light.png";
-var logoDarkFooter = "../../assets/img/telecall-logo6-dark.png";
-
-function switchLogoFooter() {
-  document.getElementById("logo-footer").src = logoDarkFooter;
-  let aux = logoDarkFooter;
-  logoDarkFooter = logoLightFooter;
-  logoLightFooter = aux;
-}
-
 // up to top button
 window.onscroll = function () { showTopButton() };
 
@@ -42,4 +12,97 @@ function showTopButton() {
 function upToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// dark-mode
+const chk = document.getElementById("chk")
+
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode");
+  switchLogo();
+  switchIcons();
+})
+
+
+// switchLogo
+var logoLightHeader = "../../assets/img/telecall-logo6-light.png";
+var logoDarkHeader = "../../assets/img/telecall-logo6-dark.png";
+
+var logoLightFooter = "../../assets/img/telecall-logo6-light.png";
+var logoDarkFooter = "../../assets/img/telecall-logo6-dark.png";
+
+function switchLogo() {
+  document.getElementById("logo-header").src = logoDarkHeader;
+  let auxHeader = logoDarkHeader;
+  logoDarkHeader = logoLightHeader;
+  logoLightHeader = auxHeader;
+
+  document.getElementById("logo-footer").src = logoDarkFooter;
+  let auxFooter = logoDarkFooter;
+  logoDarkFooter = logoLightFooter;
+  logoLightFooter = auxFooter;
+}
+
+// switchIcons
+var internetIconLightMode = "../../assets/icons/internet-icon-light.png";
+var internetIconDarkMode = "../../assets/icons/internet-icon-dark.png";
+
+var cpaasIconLightMode = "../../assets/icons/cpaas-icon-light.png";
+var cpaasIconDarkMode = "../../assets/icons/cpaas-icon-dark.png";
+
+var mobilityIconLightMode = "../../assets/icons/mobility-icon-light.png";
+var mobilityIconDarkMode = "../../assets/icons/mobility-icon-dark.png";
+
+var smartphoneIconLightMode = "../../assets/icons/smartphone-icon-dark.png";
+var smartphoneIconDarkMode = "../../assets/icons/smartphone-icon-light.png";
+
+var companyIconLightMode = "../../assets/icons/company-icon-dark.png";
+var companyIconDarkMode = "../../assets/icons/company-icon-light.png";
+
+var networkIconLightMode = "../../assets/icons/network-icon-dark.png";
+var networkIconDarkMode = "../../assets/icons/network-icon-light.png";
+
+var serviceIconLightMode = "../../assets/icons/service-icon-dark.png";
+var serviceIconDarkMode = "../../assets/icons/service-icon-light.png";
+
+function switchIcons() {
+  document.getElementById("internetIcon").src = internetIconDarkMode;
+  let auxinternet = internetIconDarkMode;
+  internetIconDarkMode = internetIconLightMode;
+  internetIconLightMode = auxinternet;
+
+  document.getElementById("cpaasIcon").src = cpaasIconDarkMode;
+  let auxcpaas = cpaasIconDarkMode;
+  cpaasIconDarkMode = cpaasIconLightMode;
+  cpaasIconLightMode = auxcpaas;
+
+  document.getElementById("mobilityIcon").src = mobilityIconDarkMode;
+  let auxmobility = mobilityIconDarkMode;
+  mobilityIconDarkMode = mobilityIconLightMode;
+  mobilityIconLightMode = auxmobility;
+
+  document.getElementById("chekListIcon").src = checkListIconDarkMode;
+  let auxcheckList = checkListIconDarkMode;
+  checkListIconDarkMode = checkListIconLightMode;
+  checkListIconLightMode = auxcheckList;
+
+  document.getElementById("smartphoneIcon").src = smartphoneIconDarkMode;
+  let auxsmartphone = smartphoneIconDarkMode;
+  smartphoneIconDarkMode = smartphoneIconLightMode;
+  smartphoneIconLightMode = auxsmartphone;
+
+  document.getElementById("companyIcon").src = companyIconDarkMode;
+  let auxcompany = companyIconDarkMode;
+  companyIconDarkMode = companyIconLightMode;
+  companyIconLightMode = auxcompany;
+
+  document.getElementById("networkIcon").src = networkIconDarkMode;
+  let auxnetwork = networkIconDarkMode;
+  networkIconDarkMode = networkIconLightMode;
+  networkIconLightMode = auxnetwork;
+
+  document.getElementById("serviceIcon").src = serviceIconDarkMode;
+  let auxservice = serviceIconDarkMode;
+  serviceIconDarkMode = serviceIconLightMode;
+  serviceIconLightMode = auxservice;
 }
