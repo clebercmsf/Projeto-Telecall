ceac596b732d1a55334d28015e4a7f8e8b7aa101
clebercmsf <cleber.cmsf@gmail.com>
2023-11-22 14:12:46 -0300

    fix

--- src/pages/login/validator.php
+++ src/pages/login/validator.php
@@ -8,21 +8,9 @@
 
   $result = $connect->query($sql);
 
-  if (mysqli_num_rows($result) < 1) {
-    
-  }
-
-  // $qry = $connect->prepare('INSERT INTO usuario (Nome_Usuario, Nascimento_Usuario, Sexo_Usuario, Nome_Materno, CPF_Usuario, Celular_Usuario, Telefone_Usuario, Endereco_Usuario, Login_Usuario, Senha_Usuario, Perfil_Usuario) values (:nome, :birth, :gender, :mName, :cpf, :cNumber, :tNumber, :addres, :username, :pasword, :perfil)');
-	// 	$qry->bindValue(':nome', $nome);
-	// 	$qry->bindValue(':birth', $birth);
-  //   $qry->bindValue(':gender', $gender);
-  //   $qry->bindValue(':mName', $mName);
-  //   $qry->bindValue(':cpf', $cpf);
-  //   $qry->bindValue(':cNumber', $cNumber);
-  //   $qry->bindValue(':tNumber', $tNumber);
-  //   $qry->bindValue(':addres', $address);
-  //   $qry->bindValue(':username', $username);
-  //   $qry->bindValue(':pasword', $password);
-  //   $qry->bindValue(':perfil', 2);
-	// 	$qry->execute();
+  if ($result->PDO::rowCount > 0) {
+    echo "Login bem-sucedido!";
+} else {
+    echo "Usuário ou senha incorretos.";
+}
 ?>
