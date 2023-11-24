7486b31e55250cedf8dc1758817d6bcf86375a5a
clebercmsf <cleber.cmsf@gmail.com>
2023-11-24 15:59:09 -0300

    fix

--- src/pages/crud/delete.php
+++ src/pages/crud/delete.php
@@ -1,17 +1,24 @@
 <?php
-
 if (!empty($_GET['id'])) {
-  include_once('config.php');
+  try {
+    $connect = new PDO("mysql:host=localhost;dbname=db_telecall", "root", "");
 
-  $id = $_GET['id'];
+    $id_Usuario = $_GET['id'];
+    $sqlSelect = "SELECT * FROM usuario WHERE id_Usuario = :id_Usuario";
+    $stmt = $connect->prepare($sqlSelect);
+    $stmt->bindParam(':id_Usuario', $id_Usuario, PDO::PARAM_INT);
+    $stmt->execute();
 
-  $sqlSelect = "SELECT *  FROM usuario WHERE id=$id";
-
-  $result = $conexao->query($sqlSelect);
-
-  if ($result->num_rows > 0) {
-    $sqlDelete = "DELETE FROM usuario WHERE id=$id";
-    $resultDelete = $conexao->query($sqlDelete);
+    if ($stmt->rowCount() > 0) {
+      $sqlDelete = "DELETE FROM usuario WHERE id_Usuario=:id_Usuario";
+      $stmtDelete = $connect->prepare($sqlDelete);
+      $stmtDelete->bindParam(':id_Usuario', $id_Usuario, PDO::PARAM_INT);
+      $stmtDelete->execute();
+    }
+  } catch (PDOException $e) {
+    echo "Erro na conexão com o banco de dados: " . $e->getMessage();
+    exit;
   }
 }
+
 header('Location: sistema.php');
--- src/pages/crud/sistema.php
+++ src/pages/crud/sistema.php
@@ -9,13 +9,14 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['profile']) || $_SESSION['
 }
 
 $logado = $_SESSION['username'];
-// if (!empty($_GET['search'])) {
-//   $data = $_GET['search'];
-//   $sql = "SELECT * FROM usuario WHERE id LIKE '%$data%' or nome LIKE '%$data%' or username LIKE '%$data%' ORDER BY id_Usuario DESC";
-// } else {
-//   $sql = "SELECT * FROM usuario ORDER BY id_Usuario DESC";
-// }
-$sql = "SELECT * FROM usuario ORDER BY id_Usuario ASC";
+if (!empty($_GET['search'])) {
+  $data = $_GET['search'];
+  $sql = "SELECT * FROM usuario WHERE id_Usuario LIKE '%$data%' or Nome_Usuario LIKE '%$data%' or Sexo_Usuario LIKE '%$data%' or CPF_Usuario LIKE '%$data%' 
+  or Celular_Usuario LIKE '%$data%' or Telefone_Usuario LIKE '%$data%' or Endereco_Usuario LIKE '%$data%' or Login_Usuario LIKE '%$data%'
+  or Senha_Usuario LIKE '%$data%' or Perfil_Usuario LIKE '%$data%' ORDER BY id_Usuario ASC";
+} else {
+  $sql = "SELECT * FROM usuario ORDER BY id_Usuario ASC";
+}
 $result = $conexao->query($sql);
 ?>
 <!DOCTYPE html>
@@ -26,53 +27,38 @@ $result = $conexao->query($sql);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <title>SISTEMA | GN</title>
-  <style>
-    body {
-      background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-      color: white;
-      text-align: center;
-    }
-
-    .table-bg {
-      background: rgba(0, 0, 0, 0.3);
-      border-radius: 15px 15px 0 0;
-    }
-
-    .box-search {
-      display: flex;
-      justify-content: center;
-      gap: .1%;
-    }
-  </style>
+  <link rel="stylesheet" href="./style/sistema.css" />
+  <title>CRUD - Telecall</title>
+  <link rel="icon" href="../../assets/img/telecall-logo4-light.png" />
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="../home/home.php">USUARIOS TELECALL</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </div>
-    <div class="d-flex">
-      <a href="sair.php" class="btn btn-danger me-5">Sair</a>
-    </div>
-  </nav>
+<header>
+    <nav class="nav-bar">
+      <div class="logo">
+        <a href="../home/home.php">
+          <img src="../../assets/img/telecall-logo6-light.png" alt="telecall-logo" id="logo-header">
+        </a>
+      </div>
+      <div>
+        <button onclick='logout();'>Sair</button>
+      </div>
+    </nav>
+  </header>
   <br>
   <?php
-  echo "<h1>Bem vindo <u>$logado</u></h1>";
+  echo "<h1>BEM VINDO <u>$logado</u></h1>";
   ?>
   <br>
-  <div class="box-search">
-    <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
-    <button onclick="searchData()" class="btn btn-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <div>
+    <input type="search" class="textfield" placeholder="Pesquisar" id="search">
+    <button onclick="searchData()" class="btn btnP">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
       </svg>
     </button>
   </div>
-  <div class="m-5">
+  <div class="m-3">
     <table class="table text-white table-bg">
       <thead>
         <tr>
@@ -133,7 +119,7 @@ $result = $conexao->query($sql);
   </div>
 </body>
 <script>
-  var search = document.getElementById('pesquisar');
+  let search = document.getElementById('search');
 
   search.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
@@ -142,7 +128,11 @@ $result = $conexao->query($sql);
   });
 
   function searchData() {
-    window.location = 'sistema.php?search=' + search.value;
+    window.location = `sistema.php?search=${search.value}`;
+  }
+
+  function logout() {
+    window.location.href = '../crud/sair.php';
   }
 </script>
 
