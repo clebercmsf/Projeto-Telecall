1bacb8d356dd5d30a6a9a069dddfa62897360d67
clebercmsf <cleber.cmsf@gmail.com>
2023-11-27 13:23:11 -0300

    final

--- src/pages/crud/sistema.php
+++ src/pages/crud/sistema.php
@@ -28,7 +28,7 @@ $result = $conexao->query($sql);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="./sistema/style/sistema.css" />
-  <script src="./sistema/sistema.js"></script>
+  <script src="./sistema/sistema.js" defer></script>
   <title>CRUD - Telecall</title>
   <link rel="icon" href="../../assets/img/telecall-logo4-light.png" />
 </head>
